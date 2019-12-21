666009023998e92a1fa4cb268fbfc6dc12c88d6a
Gavin <ekkambyte@gmail.com>
2019-12-21 11:54:53 -0500

    made orm file

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
 // placeholder
+const mysql = require('mysql');
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory to be served
+app.use(express.static("app/public"));
+
+// Routes
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "",
+    database: "Employee_DB"
+  });
+  
